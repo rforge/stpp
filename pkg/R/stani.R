74274c3c7709bb5fa46c9669b15b959a7bd86e98
barryrowlingson <barryrowlingson@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-24 10:46:05 +0000

    stani now uses states for rendering, updated docs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stpp@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stani.R
+++ pkg/R/stani.R
@@ -4,12 +4,10 @@
   par3d(skipRedraw=TRUE)
   np=dim(o$xyt)[1]
 
-  ## what's in our window?
-  if(!o$persist){
-    tin=o$xyt[,3]>(o$t-o$width) & o$xyt[,3] < (o$t)
-  }else{
-    tin= o$xyt[,3] < (o$t+o$width)
-  }  
+  ## compute new states
+  tin = rep(1,np)
+  tin[o$xyt[,3]>(o$t-o$width)]=2
+  tin[o$xyt[,3]>o$t]=3
 
   ## which points have changed state since last time?
   changed = tin != o$xyt[,5]
@@ -21,15 +19,9 @@
 
   ## now add them back in their correct state:
   for(i in (1:np)[changed]){
-    if(tin[i]){
-      material3d(o$s2)
-      o$xyt[i,4]=spheres3d(x=o$xyt[i,1],y=o$xyt[i,2],z=o$xyt[i,3],radius=o$s2$radius)
-      o$xyt[i,5]=tin[i]
-    }else{
-      material3d(o$s1)
-      o$xyt[i,4]=spheres3d(x=o$xyt[i,1],y=o$xyt[i,2],z=o$xyt[i,3],radius=o$s1$radius)
-      o$xyt[i,5]=tin[i]
-    }
+    material3d(o$states[[tin[i]]])
+    o$xyt[i,4]=spheres3d(x=o$xyt[i,1],y=o$xyt[i,2],z=o$xyt[i,3],radius=o$states[[tin[i]]]$radius)
+    o$xyt[i,5]=tin[i]
   }
   ## start drawing again:
   par3d(skipRedraw=FALSE)
@@ -38,12 +30,11 @@
   return(o)
 }
 
-.rp.stan3d <- function(xyt,tlim,twid,persist,states) {
+.rp.stan3d <- function(xyt,tlim,twid,states) {
   t=tlim[1];width=twid
   stan.panel  <- rp.control(title="space-time animation",
-                            xyt=xyt, t=tlim[1], width=twid, persist=persist,
-                            s1=states$s1,
-                            s2=states$s2
+                            xyt=xyt, t=tlim[1], width=twid,
+                            states=states
                             )
   rp.slider(stan.panel, t, title = "time", from=tlim[1], to=tlim[2], action = .stan3d.redraw,showvalue=TRUE)
   rp.slider(stan.panel, width, title = "window", from=0, to=diff(tlim), action = .stan3d.redraw,showvalue=TRUE)
@@ -56,11 +47,16 @@ stani=function(xyt,tlim=range(xyt[,3],na.rm=TRUE),twid=diff(tlim)/20,persist=FAL
   require(rgl)
   require(rpanel)
   if(missing(states)){
+    ## default colouring scheme:
     states=list(
-      s1=list(col="blue",radius=1/80),
-      s2=list(col="red",radius=1/20),
-      s3=list(col="yellow",radius=1/40)
+      s1=list(col="blue",radius=1/80,alpha=1.0),
+      s2=list(col="red",radius=1/20,alpha=1.0),
+      ## still-to-come points are invisible (alpha=0)
+      s3=list(col="yellow",alpha=0.0,radius=1/80)
       )
+    if(persist){
+      states$s1=states$s2
+    }
   }
   xyt=data.frame(xyt)
   xyt$id=NA
@@ -81,7 +77,7 @@ stani=function(xyt,tlim=range(xyt[,3],na.rm=TRUE),twid=diff(tlim)/20,persist=FAL
   for(i in 1:(dim(xyt)[1])){
     xyt[i,4]=points3d(xyt[,1],xyt[,2],xyt[,3])
   }
-  .rp.stan3d(xyt,tlim,twid,persist,states)
+  .rp.stan3d(xyt,tlim,twid,states)
 }
 
 .ranger=function(x,margin=0.2){
