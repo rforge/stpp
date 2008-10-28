81031143eeec2493d81087878c73676451f1add7
gabriele <gabriele@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-28 12:07:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stpp@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/animation.r
+++ pkg/R/animation.r
@@ -1,4 +1,4 @@
-animation <- function(xyt, s.region, t.region, runtime=1, incident="red", prevalent="pink3", fade=0.01, pch=19, cex=0.25, plot.s.region=T, scales=T, border.frac=0.05, add=F)
+animation <- function(xyt, s.region, t.region, runtime=1, incident="red", prevalent="pink3", pch=19, cex=0.35, plot.s.region=T, scales=T, border.frac=0.05, add=F)
 { 
   #
   # Description:
@@ -8,15 +8,14 @@ animation <- function(xyt, s.region, t.region, runtime=1, incident="red", preval
   # Arguments:
   #            xyt: data - matrix containing the (x,y,t)-coordinates
   #        runtime: approximate running time of animation, in seconds
+  # 			  (but longer than expected...).	
   #       s.region: two-column matrix specifying polygonal
   #                 region containing all data-locations xyt[,1:2]
   #       t.region: interval containing all data-times xyt[,3]
   #       incident: colour in which incident point xyt[i,1:2] is
   #                 plotted at time xyt[i,3]
-  #      prevalent: colour to which prevalent point xyt[1,1:2] fades
-  #                 at time xyt[i,3]+fade
-  #           fade: approximate time-delay for colour-change from incident
-  #                 to prevalent, in data-time units
+  #      prevalent: colour to which prevalent point xyt[i,1:2] fades
+  #                 at time xyt[i+1,3]
   #            pch: plotting symbol usedfor each point 
   #            cex: magnification of plotting symbol relative to standard size
   #  plot.s.region: if true, plot s.region as polygon
@@ -30,7 +29,6 @@ animation <- function(xyt, s.region, t.region, runtime=1, incident="red", preval
   if (missing(s.region)) s.region <- sbox(xyt[,1:2],xfrac=0.01,yfrac=0.01)
   if (missing(t.region)) t.region <- range(xyt[,3],na.rm=T)
   
-#  par(pty="s",mfrow=c(1,1))
   ott<-order(xyt[,3])
   sxyt<-xyt[ott,]
   rangex<-range(s.region[,1])
@@ -40,7 +38,7 @@ animation <- function(xyt, s.region, t.region, runtime=1, incident="red", preval
   xy<-as.matrix(sxyt[,1:2])
   tt<-sxyt[,3]
   npts<-length(tt)
-  T0 <- max(t.region)
+  T0=length(unique(tt))
 
   if (add==F)
     {
@@ -53,19 +51,23 @@ animation <- function(xyt, s.region, t.region, runtime=1, incident="red", preval
     }
   nplotted<-0
   tt.now<-0
-  tt.fade<-0
+  tt.fade<-0	
   while (nplotted<npts)
     {
       i<-nplotted+1
       tt.gap<-tt[i]-tt.now
-      junk<-Sys.sleep((tt.gap/T0)*runtime)
-      n.fade<-sum(tt[1:i]<tt.fade)
+	if (!(is.null(runtime)))
+      	junk<-Sys.sleep((tt.gap/T0)*runtime)
+      n.fade<-sum(tt[1:i]<=tt.fade)
       if (sum(n.fade)>0)
         points(xy[1:n.fade,1],xy[1:n.fade,2],col=prevalent,pch=19,cex=cex)
-      points(xy[i,1],xy[i,2],col=incident,pch=19,cex=cex)
-      nplotted<-i
+
+	M=which(tt==tt[i])
+	points(xy[M,1],xy[M,2],col=incident,pch=19,cex=cex)
+      nplotted<-nplotted+length(M)
       tt.now<-tt[i]
-      tt.fade<-tt.now-fade
+      tt.fade<-tt.now
     }
 }
+
 
